e8e2bae60bbd5641f8421bbf0156f1001925c7cc
Bruno Bodin <bruno.bodin@cea.fr>
2017-02-04 09:27:44 +0100

    build czmq

--- build.bat
+++ build.bat
@@ -1,14 +1,30 @@
 : build script on windows
+SET CURRENTDIR=%cd%
 
 SET CMAKE="c:\program files (x86)\cmake\bin\cmake"
 
 : setup visual studio 2015
-: call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 
 : configure zmq
 : -------------
 mkdir "build_dir\zmq"
 cd "build_dir\zmq"
-%cmake% -G "Visual Studio 14 2015 Win64" ..\..\..\libzmq
+%cmake% -G "Visual Studio 14 2015 Win64" -DZMQ_BUILD_TESTS=OFF ..\..\..\libzmq
+msbuild ZeroMQ.sln /p:Configuration=Release
+.\bin\Release\inproc_lat.exe 100 100
+
+cd ..\..
+
+: configure czmq
+: --------------
+set LIBZMQ_INCLUDE_DIRS=%CURRENTDIR%\..\libzmq\include
+set LIBZMQ_LIBRARIES=%CURRENTDIR%\build_dir\zmq\lib\Release\libzmq-v140-mt-4_2_2.lib
+
+mkdir "build_dir\czmq"
+cd "build_dir\czmq"
+%cmake% -G "Visual Studio 14 2015 Win64"  ..\..\..\czmq -DLIBZMQ_INCLUDE_DIRS=%LIBZMQ_INCLUDE_DIRS% -DLIBZMQ_LIBRARIES=%LIBZMQ_LIBRARIES%
+msbuild czmq.sln /p:Configuration=Release
+
 
 pause -1
